6fa6168abf2a6f4b25cf14bab0e0ba325beb319a
juyeon99 <beive999@naver.com>
2024-06-10 17:45:24 +0900

    Upload 03_WHERE_HW.sql

---
+++ 03_WHERE_HW.sql
@@ -0,0 +1,74 @@
+-- EMPLOYEE 테이블에서 DEPT_CODE가 'D1'인 데이터를 조회하시오.
+SELECT *
+FROM employee
+WHERE DEPT_CODE = 'D1';
+
+-- EMPLOYEE 테이블에서 SALARY가 5000000 이상인 데이터를 조회하시오.
+SELECT *
+FROM employee
+WHERE SALARY >= 5000000;
+
+-- EMPLOYEE 테이블에서 DEPT_CODE가 'D1'이면서 SALARY가 3000000 이상인 데이터를 조회하시오.
+SELECT *
+FROM employee
+WHERE DEPT_CODE = 'D1' AND SALARY > 3000000;
+
+-- EMPLOYEE 테이블에서 DEPT_CODE가 'D1'이거나 JOB_CODE가 'J4'인 데이터를 조회하시오.
+SELECT *
+FROM employee
+WHERE DEPT_CODE = 'D1' OR JOB_CODE = 'J4';
+
+-- EMPLOYEE 테이블에서 SALARY가 2000000 이상 4000000 이하인 데이터를 조회하시오.
+SELECT *
+FROM employee
+WHERE SALARY BETWEEN 2000000 AND 4000000;
+
+-- EMPLOYEE 테이블에서 EMP_NAME에 '김'이 포함된 데이터를 조회하시오.
+SELECT *
+FROM employee
+WHERE EMP_NAME LIKE '%김%';
+
+-- EMPLOYEE 테이블에서 EMP_NAME이 '이'로 시작하는 데이터를 조회하시오.
+SELECT *
+FROM employee
+WHERE EMP_NAME LIKE '이%';
+
+-- EMPLOYEE 테이블에서 EMAIL이 'greedy.com'으로 끝나는 데이터를 조회하시오.
+SELECT *
+FROM employee
+WHERE EMAIL LIKE '%greedy.com';
+
+-- EMPLOYEE 테이블에서 EMP_NAME이 '송'으로 시작하고 '기'로 끝나는 데이터를 조회하시오.
+SELECT *
+FROM employee
+WHERE EMP_NAME LIKE '송%기';
+
+-- EMPLOYEE 테이블에서 PHONE이 '010'으로 시작하지 않는 데이터를 조회하시오.
+SELECT *
+FROM employee
+WHERE PHONE NOT LIKE '010%';
+
+-- EMPLOYEE 테이블에서 DEPT_CODE가 'D1', 'D2', 'D3'인 데이터를 조회하시오.
+SELECT *
+FROM employee
+WHERE DEPT_CODE IN ('D1','D2','D3');
+
+-- EMPLOYEE 테이블에서 JOB_CODE가 'J1', 'J2', 'J3'이 아닌 데이터를 조회하시오.
+SELECT *
+FROM employee
+WHERE JOB_CODE NOT IN ('J1','J2','J3');
+
+-- EMPLOYEE 테이블에서 SAL_LEVEL이 'S1', 'S2', 'S3'인 데이터를 조회하시오.
+SELECT *
+FROM employee
+WHERE SAL_LEVEL IN ('S1','S2','S3');
+
+-- EMPLOYEE 테이블에서 DEPT_CODE가 NULL인 데이터를 조회하시오.
+SELECT *
+FROM employee
+WHERE DEPT_CODE IS NULL;
+
+-- EMPLOYEE 테이블에서 EMAIL이 NULL이 아닌 데이터를 조회하시오.
+SELECT *
+FROM employee
+WHERE EMAIL IS NOT NULL;
